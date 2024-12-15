d843e3656db5c1930b97c4eaa3f35bb81927ef81
Emilia Afandieva <153540378+afandieva@users.noreply.github.com>
2024-12-15 19:41:21 +0400

    lcp.cpp

--- kr4.cpp
+++ kr4.cpp
@@ -1,24 +1,46 @@
-#include <vector>
 #include <iostream>
-template <typename T>
-void Duplicate(std::vector<T>& v)
-{
-    v.reserve(v.size());
-	for(auto it=v.begin(); it!=v.end();++it)
-	{
-		v.push_back(*it);
-	}
+#include <string>
+#include <vector>
 
+std::string LCP(const std::vector<std::string>& strings) { 
+    if (strings.empty()) return "";  
+
+    size_t lenght = strings[0].size();
+    for (size_t i = 1; i < strings.size(); ++i) {
+        const std::string& elem = strings[i];
+        size_t j = 0;
+
+        while (j < lenght && j < elem.size() && strings[0][j] == elem[j]) {
+            ++j;
+        }
+        lenght = j; 
+        if (lenght == 0) return "";  
+    }
+
+    return strings[0].substr(0, lenght); 
 }
 
-int main()
-{
-	std::vector<int> v{1,2,3};
-	Duplicate(v);
-	
-	for(const auto& el: v)
-	{
-		std::cout<< el << ' ';
-	}
-return 0;
+int main() {
+    std::vector<std::string> mas;
+    int n;
+    std::string str;
+
+    std::cout << "Введите количество строк:\n";
+    std::cin >> n;
+    std::cin.ignore();  
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << "Введите строку №" << i + 1 << ":\n";
+        std::getline(std::cin, str);
+        mas.push_back(str);
+    }
+
+    std::string lsp = LCP(mas);
+    if (lsp.empty()) { 
+        std::cout << "Общего префикса нет.\n";
+    } else {
+        std::cout << "Наибольший общий префикс: '" << lsp << "'\n";
+    }
+
+    return 0;
 }
